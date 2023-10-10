127147b1d8bfcb13b59fbbe6efee5fd7861b7ea6
Sylvernus Akubo <akubosylvernus@gmail.com>
2023-10-10 10:33:39 +0100

    :tada:

--- dist/channels/index.d.ts
+++
@@ -1,8 +0,0 @@
-import { Base } from "../base";
-import { CreateChannelRequest, CreateChannelResponse, GetChannelByNameRequest, GetChannelByNameResponse, GetChannelListRequest, GetChannelListResponse, UpdateChannelRequest, UpdateChannelResponse } from "./type";
-export declare class Channels extends Base {
-    createChannel(channel: CreateChannelRequest): Promise<CreateChannelResponse>;
-    updateChannel(channel: UpdateChannelRequest): Promise<UpdateChannelResponse>;
-    getChannelByName(channel: GetChannelByNameRequest): Promise<GetChannelByNameResponse>;
-    getChannels(params: GetChannelListRequest): Promise<GetChannelListResponse>;
-}
--- dist/index.d.ts
+++
@@ -1,7 +0,0 @@
-import { Base } from "./base";
-import { Channels } from "./channels";
-declare class Websuckit extends Base {
-}
-interface Websuckit extends Channels {
-}
-export default Websuckit;
