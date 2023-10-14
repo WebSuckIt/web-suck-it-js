02bd237f5794e5b9ff6dfbdaeb014945ca570b5a
Sylvernus Akubo <akubosylvernus@gmail.com>
2023-10-14 22:24:00 +0100

    FEAT: channel connection

--- example/src/execute.ts
+++
@@ -1,9 +0,0 @@
-import Websuckit from "@websuckit/js";
-
-const ws = new Websuckit({ userId: "testing" }).connect({
-  publicKey: "string",
-  channelName: "string",
-  channelPassKey: "string",
-  userId: "string",
-});
-console.log({ ws });
